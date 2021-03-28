ea7603cde204b503a73c8390eb4a471746b17380
rxchan <rxchan@umich.edu>
2021-03-28 01:44:19 -0400

    written insert/contains with multihashing strategy of splitting md5 hash into n groups of bits, need to test still

--- BloomFilter.h
+++ BloomFilter.h
@@ -5,7 +5,8 @@
 #include <cmath>
 #include <string>
 #include <string.h>
-#include <openssl/md5.h>
+// #include "/opt/homebrew/opt/openssl@1.1"
+#include <openssl>
 
 class Bloomfilter {
 public:
@@ -19,31 +20,64 @@ public:
 
     void insert(const std::string& s) {
         //Hash the string into two unique hashes
-
+        std::pair<uint64_t, uint64_t> hashes = hash(s);
+        int hashSize = 64 / num_hashes; // split into num_hashes sets of bits for each "hash function"
+        // setup mask for getting each set of bits
+        int mask = 0;
+        int to_add = 1;
+        for (int i = 0; i < hashSize; i++) {
+            mask += to_add;
+            to_add *= 2;
+        }
         //Use double hashing to get unique bit, and repeat for each hash function
-
+        for (int i = 0; i < num_hashes; i++) {
+            // get lowest (hashSize) bits from both parts of hash
+            int first = (hashes->first) & mask; 
+            int second = (hashes->second) & mask; 
+            // shift hashes 
+            hashes->first = hashes->first >> (i * hashSize);
+            hashes->second = hashes->second >> (i * hashSize);
+            //compute index to set
+            int to_set = (first + (i * second)) % container.size();
+            container[to_set] = true;
+        }
     }
 
     bool contains(const std::string& s) {
-        //Hash the string into two unqiue hashes
-
+        //Hash the string into two unique hashes
+        std::pair<uint64_t, uint64_t> hashes = hash(s);
+        int hashSize = 64 / num_hashes; // split into num_hashes sets of bits for each "hash function"
+        // setup mask for getting each set of bits
+        int mask = 0;
+        int to_add = 1;
+        for (int i = 0; i < hashSize; i++) {
+            mask += to_add;
+            to_add *= 2;
+        }
         //Use double hashing to get unique bit, and repeat for each hash function
-        //If bit is false, we know for certain this unique string has not been inserted
-
-        //If all bits were true, the string is likely inserted, but false positive is possible
-
-        //This line is for compiling, replace this with own code
-        return false;
+        for (int i = 0; i < num_hashes; i++) {
+            // get lowest (hashSize) bits from both parts of hash
+            int first = (hashes->first) & mask; 
+            int second = (hashes->second) & mask; 
+            // shift hashes 
+            hashes->first = hashes->first >> (i * hashSize);
+            hashes->second = hashes->second >> (i * hashSize);
+            //compute index to set
+            int to_check = (first + (i * second)) % container.size();
+            if (!container[to_check])
+                return false;
+        }
+        //all hash function indexes true, search term probably in bloom filter
+        return true;
     }
 
 private:
-    //Add any private member variables that may be neccessary
     int num_hashes;
     std::vector<bool> container;
     std::pair<uint64_t, uint64_t> hash(const std::string& datum) {
         //Use MD5 to hash the string into one 128 bit hash, and split into 2 hashes
         unsigned char val[16];
-        MD5(datum.c_str(), datum.length(), val);
+        MD5((const unsigned char *)datum.c_str(), datum.length(), val);
         uint64_t first, second;
         memcpy(&first, val, 8);
         memcpy(&second, val + 8, 8);
